c2fad1ac2d2361b1359c8fe6f0ecb597080e4e8a
Cameron Jacobsen <cameronjacobsen1193@gmail.com>
2025-08-11 12:27:05 -0400

    Added handleClick in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,23 @@
+import React, { useState } from 'react';
 import './SearchBar.css';
 
 function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchLocation, setSearchLocation] = useState('');  
+    const [sortOption, setSortOption] = useState('Best Match');
+
+    const handleClick = (e) => {
+        setSortOption(e.currentTarget.value);
+        document.querySelectorAll('.filters button').forEach(b => (b.style.border = 'none'));
+        e.currentTarget.style.border = '1px solid white';
+        };
+
     return (
         <div className='search-bar'>
             <div className='filters'>
-                <button>Best Match</button>
-                <button>Highest Rated</button>
-                <button>Most Reviewed</button>
+                <button value='Best Match' onClick={handleClick}>Best Match</button>
+                <button value='Highest Rated' onClick={handleClick}>Highest Rated</button>
+                <button value='Most Reviewed' onClick={handleClick}>Most Reviewed</button>
             </div>
             <div className='inputs'>
                 <input name='business' placeholder='Seach Businesses'/>
